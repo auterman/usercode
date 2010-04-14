0ba8daee6b97ec833b803bb6f1272a5ea91ed88e
auterman <>
2010-04-14 15:45:22 +0000

    *** empty log message ***

--- Analysis/FinalPlots/python/finalplots_cfi.py
+++ Analysis/FinalPlots/python/finalplots_cfi.py
@@ -6,10 +6,18 @@ finalPlot = cms.EDAnalyzer('FinalPlots',
      uncertainty_name = cms.string(  ''),
      weightName       = cms.InputTag('weight'),
      JetPtMin         = cms.double( 30.0 ),
-     JetEtaMax        = cms.double( 2.5 ),
+     JetEtaMax        = cms.double( 2.4 ),
      #
      Towers           = cms.InputTag("towerMaker"),
      Tracks           = cms.InputTag("ctfWithMaterialTracks"),
      GroupNTowers     = cms.int32( 3 ),
+     TowerDeltaEtaMax = cms.double( 0.3 ),
+     TowerDeltaPhiMax = cms.double( 0.3 ),
+     #
+     TrackEtaMax      = cms.double( 2.4 ),
+     TrackPtMin       = cms.double( 0.0 ),
+     TrackChiSquareMax= cms.double( 8.0 ),
+     TrackNumOfHitsMin= cms.int32( 10 ),
+     TrackDeltaRMin   = cms.double( 0.5 ),
 )
 
