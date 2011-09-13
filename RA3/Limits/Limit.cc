e01454f2494fcd50defad2f3a5d4c7eebde52228
gebbert <>
2011-09-13 14:10:27 +0000

    inclusion of limit calculation for 2j selection

--- RA3/Limits/Limit.cc
+++ RA3/Limits/Limit.cc
@@ -34,9 +34,16 @@ void Limit(int argc, char *argv[])
     double slum    = config.read<double>("Luminosity.uncertainty");
     double NLOXsection = config.read<double>("NLOXsection",1.0);
     double kfactor  = config.read<double>("kfactor",1.0);
+    //additional values for 2j selection
+    int    n2j       = config.read<int>("data2j");
+       double bck2j    = config.read<double>("background2j");
+       double sbck2j    = config.read<double>("background.uncertainty2j");
+       double eff2j     = config.read<double>("signal.acceptance2j");
+       double seff2j    = config.read<double>("signal.acceptance.uncertainty2j");
+       double sig_cont2j= config.read<double>("signal.contamination2j");
 
     int ntoys = 2000;
-    int nuisanceModel = 1; //0 = Gauss, 1=lognormal
+    int nuisanceModel = 0; //0 = Gauss, 1=lognormal
     std::string SM = "cls"; // StatisticalModel = "bayesian", "cls", ...
 
     double effSC = eff - (sig_cont);
@@ -53,7 +60,7 @@ void Limit(int argc, char *argv[])
     LimitResult limitSigCont=roostats_limit(ilum, slum, effSC, seffSC, bck, sbck, n, false,
     		                 nuisanceModel, SM,"plot_cl95_sigcont.pdf",12345);
 
-    //LimitResult expected_limitSigCont = roostats_clm(ilum, slum, effSC, seffSC, bck, sbck, ntoys, nuisanceModel, SM);
+
 
     //limits (doing nothing about signal contamination)
     std::cout<<"======================================================================Limit w/o subtraction of Signal Contamination"<<bck<<std::endl;
@@ -61,8 +68,17 @@ void Limit(int argc, char *argv[])
            nuisanceModel,SM,"plot_cl95.pdf",12345);
 
 
+    //limits (signal contamination removed, 2-jet selection)
+    double effSC2j = eff2j - (sig_cont2j);
+        if(effSC2j<0)effSC2j=0;
+        double seffSC2j = seff2j * effSC2j / eff2j;
 
-    //LimitResult expected_limit = roostats_clm(ilum, slum, eff, seff, bck, sbck, ntoys, nuisanceModel,SM);
+        std::cout<<"Signal acceptance (2j):"<<eff2j<<" +- "<< seff2j<<std::endl;
+        std::cout<<"Signal acc - cont (2j):"<<effSC2j<<" +- "<<seffSC2j<<std::endl;
+        std::cout<<"======================================================================Limit with subtraction of Signal Contamination, 2j selection"<<bck2j<<std::endl;
+        LimitResult limitSigCont2j = roostats_limit(ilum, slum, effSC2j,
+				seffSC2j, bck2j, sbck2j, n2j, false, nuisanceModel, SM,
+				"plot_cl95_sigcont.pdf", 12345);
 
 
     config.add("limit."+SM+".observed", limit.GetObservedLimit());
@@ -84,6 +100,16 @@ void Limit(int argc, char *argv[])
     config.add("limitSC."+SM+".expected.m2sigma", limitSigCont.GetTwoSigmaLowRange() );
     config.add("limitSC."+SM+".expected.p2sigma", limitSigCont.GetTwoSigmaHighRange() );
     config.add("limitSC."+SM+".expected.2sigmaCoverage", limitSigCont.GetTwoSigmaCoverage() );
+
+    config.add("limitSC2j."+SM+".observed", limitSigCont2j.GetObservedLimit());
+       config.add("limitSC2j."+SM+".observed.error", limitSigCont2j.GetObservedLimitError());
+       config.add("limitSC2j."+SM+".expected", limitSigCont2j.GetExpectedLimit() );
+       config.add("limitSC2j."+SM+".expected.m1sigma", limitSigCont2j.GetOneSigmaLowRange() );
+       config.add("limitSC2j."+SM+".expected.p1sigma", limitSigCont2j.GetOneSigmaHighRange() );
+       config.add("limitSC2j."+SM+".expected.1sigmaCoverage", limitSigCont2j.GetOneSigmaCoverage() );
+       config.add("limitSC2j."+SM+".expected.m2sigma", limitSigCont2j.GetTwoSigmaLowRange() );
+       config.add("limitSC2j."+SM+".expected.p2sigma", limitSigCont2j.GetTwoSigmaHighRange() );
+       config.add("limitSC2j."+SM+".expected.2sigmaCoverage", limitSigCont2j.GetTwoSigmaCoverage() );
 
     //write stuff:  
     time_t rawtime;
