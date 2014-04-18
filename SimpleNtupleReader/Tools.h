ae58c8013be726cb21e6cd5277749d0ba77a4e3c
Christian Autermann <auterman@cern.ch>
2014-04-18 22:27:35 +0200

    auto-commit by Makefile

--- SimpleNtupleReader/Tools.h
+++ SimpleNtupleReader/Tools.h
@@ -179,7 +179,7 @@ class Yields{
       /// QCD Reweighting binning definition
       ///
       /// ------------------------------------------------------------
-      //binning_["photon_ptstar"] = new Binnings(bins_50_0_1000, n_50+1);
+      binning_["photon_ptstar"] = new Binnings(bins_50_0_1000, n_50+1);
       binning_["recoil_pt"] = new Binnings(bins_50_0_1500, n_50+1);
       //binning_["phi_met_em1"] = new Binnings(bins_64_nPi_Pi, n_64+1);
       //binning_["phi_mht_em1"] = new Binnings( bins_64_nPi_Pi, n_64+1);
@@ -201,7 +201,11 @@ class Yields{
       //int bin = binning_["photon_ptstar"]->GetBin( g_pt );
       int bin = binning_["recoil_pt"]->GetBin( Recoil_pt(  &recoil ) );
       //bin += binning_["photon_ptstar"]->GetNBins() * binning_["recoil_pt"]->GetBin( Recoil_pt(  &recoil ) );
-      bin += binning_["recoil_pt"]->GetNBins() * binning_["phi_recoil_em1"]->GetBin( DeltaPhi( Recoil_phi( &recoil ), g_phi) );
+      bin += binning_["recoil_pt"]->GetNBins() * 
+             binning_["phi_recoil_em1"]->GetBin( DeltaPhi( Recoil_phi( &recoil ), g_phi) );
+      bin += binning_["recoil_pt"]->GetNBins() * 
+             binning_["phi_recoil_em1"]->GetNBins() * 
+             binning_["photon_ptstar"]->GetBin( g_pt );
       return bin;
     }
     virtual int GetNBins(){
