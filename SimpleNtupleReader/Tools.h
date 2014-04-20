121e7e6a88f908bf89326b6ee67e564b4037e989
Christian Autermann <auterman@cern.ch>
2014-04-20 22:59:02 +0200

    auto-commit by Makefile

--- SimpleNtupleReader/Tools.h
+++ SimpleNtupleReader/Tools.h
@@ -184,8 +184,8 @@ class Yields{
       binning_["photon_ptstar"] = new Binnings(bins_50_0_1000, n_50+1);
       //binning_["recoil_pt"] = new Binnings(bins_50_0_1500, n_50+1);
       //binning_["phi_met_em1"] = new Binnings(bins_64_nPi_Pi, n_64+1);
-      binning_["phi_mht_em1"] = new Binnings( bins_64_nPi_Pi, n_64+1);
-      //binning_["phi_mht_recoil"] = new Binnings( bins_64_nPi_Pi, n_64+1);
+      //binning_["phi_mht_em1"] = new Binnings( bins_64_nPi_Pi, n_64+1);
+      binning_["phi_mht_recoil"] = new Binnings( bins_64_nPi_Pi, n_64+1);
       //binning_["phi_recoil_em1"] = new Binnings( bins_64_nPi_Pi, n_64+1);
       
 //      double single_bin[0] = {};
@@ -219,8 +219,12 @@ class Yields{
 //      bin += binning_["phi_mht_em1"]->GetNBins() * 
 //             binning_["phi_mht_recoil"]->GetBin( DeltaPhi(mht_phi, Recoil_phi( &recoil )) );
 
-      int bin = binning_["phi_mht_em1"]->GetBin( DeltaPhi(mht_phi, g_phi) );
-      bin += binning_["phi_mht_em1"]->GetNBins() * 
+//      int bin = binning_["phi_mht_em1"]->GetBin( DeltaPhi(mht_phi, g_phi) );
+//      bin += binning_["phi_mht_em1"]->GetNBins() * 
+//             binning_["photon_ptstar"]->GetBin( g_pt );
+
+      int bin = binning_["phi_mht_recoil"]->GetBin( DeltaPhi(mht_phi, Recoil_phi( &recoil )) );
+      bin += binning_["phi_mht_recoil"]->GetNBins() * 
              binning_["photon_ptstar"]->GetBin( g_pt );
 
 //std::cout <<" DONE   virtual int GetBin(float met,float metPhi,float ht,float Sig," <<std::endl;
