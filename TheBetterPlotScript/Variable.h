e4fb6bed61f8a51d20a8d52004f2329dc5503a16
Knut <maximilian.kiesel@rwth-aachen.de>
2014-07-22 10:42:15 +0200

    c++ indend style

--- TheBetterPlotScript/Variable.h
+++ TheBetterPlotScript/Variable.h
@@ -16,37 +16,49 @@ class Variable;
 class Info;
 
 ///class Variables contains one value like cross-section and a link to static general information of this variable
-class Variable{
- public:
-  Variable():value_(0),info_(0){}
-  Variable(double value, Info*info):value_(value),info_(info){}
-  double  GetValue() const {return value_;}
-  void    SetValue(double  v){value_=v;}
-  Info *  GetInfo() const {return info_;}
-  void    SetInfo(Info* i){info_=i;}
+class Variable {
+public:
+    Variable():value_(0),info_(0) {}
+    Variable(double value, Info*info):value_(value),info_(info) {}
+    double  GetValue() const {
+        return value_;
+    }
+    void    SetValue(double  v) {
+        value_=v;
+    }
+    Info *  GetInfo() const {
+        return info_;
+    }
+    void    SetInfo(Info* i) {
+        info_=i;
+    }
 
- private:
-  double value_;
-  Info * info_;
+private:
+    double value_;
+    Info * info_;
 };
- 
+
 ///helper class containing general infromation like the name of the variables. This is supposed to be static.
-class Info{
- public:
-   Info(const std::string& name, const std::string& name_in_datacard):
-        name(name),name_in_datacard(name_in_datacard),default_value(0),use_default(false),label_(""){}
-   Info(const std::string& name, const std::string& name_in_datacard, double default_value):
-        name(name),name_in_datacard(name_in_datacard),default_value(default_value),use_default(true),label_(""){}
-   const std::string name;
-   const std::string name_in_datacard;
-   const double default_value;
-   const bool use_default;
-   
-   void Fill(Variable& v, ConfigFile& c);
-   std::string GetLabel(){return label_;}
-   void SetLabel(std::string l){label_=l;}
- private:
-   std::string label_;  
+class Info {
+public:
+    Info(const std::string& name, const std::string& name_in_datacard):
+        name(name),name_in_datacard(name_in_datacard),default_value(0),use_default(false),label_("") {}
+    Info(const std::string& name, const std::string& name_in_datacard, double default_value):
+        name(name),name_in_datacard(name_in_datacard),default_value(default_value),use_default(true),label_("") {}
+    const std::string name;
+    const std::string name_in_datacard;
+    const double default_value;
+    const bool use_default;
+
+    void Fill(Variable& v, ConfigFile& c);
+    std::string GetLabel() {
+        return label_;
+    }
+    void SetLabel(std::string l) {
+        label_=l;
+    }
+private:
+    std::string label_;
 };
 
 Info* GetInfo(const std::string&);
